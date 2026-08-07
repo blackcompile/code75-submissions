9577867a2c1d6db16d8019f118b5dd57b5453ca4
blackcompile <nusratmaya.jahan7@gmail.com>
2026-08-07 22:36:42 +0600

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/count-connected-components/submission-0.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    void dfs(int node,vector<vector<int>>&adj,vector<bool>&visited){
+        visited[node]=true;
+        for(int ni:adj[node]){
+            if(!visited[ni]){
+                dfs(ni,adj,visited);
+            }
+        }
+    }
+   int countComponents(int n,vector<vector<int>>& edges){
+      //if(edges.size()!=n-1)
+      //return false;
+      vector<vector<int>>adj(n);
+      for(auto edge:edges){
+        int u=edge[0];
+        int v=edge[1];
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+              }
+              vector<bool>visited(n,false);
+              int count=0;
+              for(int i=0;i<n;i++){
+                if(!visited[i]){
+                    count++;
+                    dfs(i,adj,visited);
+                }
+              }
+              return count;
+    }
+};
